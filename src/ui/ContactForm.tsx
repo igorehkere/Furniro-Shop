8ede8136f69755aadf7b1ea66ad8c41c040c60ef
Igorechkere <igorshvedenko@yandex.ru>
2026-04-13 22:10:40 +0300

    update contact page, add state

--- src/ui/ContactForm.tsx
+++ src/ui/ContactForm.tsx
@@ -1,44 +1,60 @@
 import style from './ContactInform.module.css'
+import { useState } from 'react'
 
 export function ContactForm () {
+
+    const [submit, setSubmit] = useState(false);
     return (
-        <div className={style.form}>
-            <div className={style.address}>
-                <div className={style.location}>
-                    <img src='./src/location.svg'/>
-                    <div className={style.dv}>
-                        <p className={style.bold}>Address</p>
-                        <p>236 5th SE Avenue, New York NY10000, United States</p>
+        <div>
+            {!submit && (
+                <div className={style.form}>
+                    <div className={style.address}>
+                        <div className={style.location}>
+                            <img src='./src/location.svg'/>
+                            <div className={style.dv}>
+                                <p className={style.bold}>Address</p>
+                                <p>236 5th SE Avenue, New York NY10000, United States</p>
+                            </div>
+                        </div>
+                        <div className={style.phone}>
+                            <img src='./src/phone.svg'/>
+                            <div className={style.dv}>
+                                <p className={style.bold}>Phone</p>
+                                <p>Mobile: +(84) 546-6789</p>
+                                <p>Hotline: +(84) 456-6789</p>
+                            </div>
+                        </div>
+                        <div className={style.clock}>
+                            <img src='./src/clock.svg'/>
+                            <div className={style.dv}>
+                                <p className={style.bold}>Working Time</p>
+                                <p>Monday-Friday: 9:00 - 22:00</p>
+                                <p>Saturday-Sunday: 9:00 - 21:00</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className={style.phone}>
-                    <img src='./src/phone.svg'/>
-                    <div className={style.dv}>
-                        <p className={style.bold}>Phone</p>
-                        <p>Mobile: +(84) 546-6789</p>
-                        <p>Hotline: +(84) 456-6789</p>
+                    <div className={style.inp}>
+                        <p>Your name</p>
+                        <input placeholder='Abc'/>
+                        <p>Email address</p>
+                        <input placeholder='Abc@def.com'/>
+                        <p>Subject</p>
+                        <input placeholder='This is an optional'/>
+                        <p>Message</p>
+                        <input placeholder="Hi! I'd like to ask about"/>
+                        <button onClick={() => {
+                            setSubmit(!submit);
+                        }}>Submit</button>
                     </div>
-                </div>
-                <div className={style.clock}>
-                    <img src='./src/clock.svg'/>
-                    <div className={style.dv}>
-                        <p className={style.bold}>Working Time</p>
-                        <p>Monday-Friday: 9:00 - 22:00</p>
-                        <p>Saturday-Sunday: 9:00 - 21:00</p>
+                </div> )}
+                {submit && (
+                    <div className={style.submit}>
+                        <p>Thanks for the request!</p>
+                        <button onClick={() => {
+                            setSubmit(!submit);
+                        }}>Back to form</button>
                     </div>
-                </div>
-            </div>
-            <div className={style.inp}>
-                <p>Your name</p>
-                <input placeholder='Abc'/>
-                <p>Email address</p>
-                <input placeholder='Abc@def.com'/>
-                <p>Subject</p>
-                <input placeholder='This is an optional'/>
-                <p>Message</p>
-                <input placeholder="Hi! I'd like to ask about"/>
-                <button>Submit</button>
-            </div>
+                )}
         </div>
     )
 }
